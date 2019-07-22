e4e8d4c7ed2633ad0f37dd49387d177900022445
18772943780@163.com <18772943780@163.com>
2019-07-22 21:56:54 +0800

    LM

---
+++ src/components/main.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="blog-main">
+      <div class="blog-header">
+        
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "main",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style lang="less">
+body{
+  margin: 0;
+  padding: 0px;
+}
+  h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.blog-main{
+  width: 100%;
+  height: 100%;
+  background: #fff;
+  & .blog-header{
+    height: 50px;
+    width: 100%;
+    background-color: #42b983;
+  }
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import main from '@/components/main'
+
+Vue.use(Router)
+
+export default new Router({
+  mode:'history',
+  routes: [
+    {
+      path: '/',
+      redirect :'/main'
+    },
+    {
+      path: '/main',
+      name: 'main',
+      component: main
+    }
+  ]
+})
